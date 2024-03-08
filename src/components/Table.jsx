4cd187609612b7b5a16c36c1061890c7fff837ec
Victor <victor.sanchez@phonr.com>
2024-03-08 15:18:11 +0100

    feat: add pagination's table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 
 import "../assets/table.scss";
 
 const Table = () => {
   const [data, setData] = useState([]);
+  const [tablePage, setTablePage] = useState(1);
+  const [pages, setPages] = useState(0);
 
   const fetchData = async () => {
     try {
@@ -15,6 +18,7 @@ const Table = () => {
       );
       const data = await res.json();
       setData(data.results);
+      setPages(Math.ceil(data.results.length / 10));
     } catch (error) {
       console.error("There was an error loading data: ", error);
     }
@@ -36,30 +40,70 @@ const Table = () => {
     <div className="table">
       <h2>Browse Questions</h2>
       {data?.length ? (
-        <table>
-          <thead>
-            <tr>
-              <th className="text-center">ID</th>
-              <th className="text-left">Category</th>
-              <th className="text-left">Type</th>
-              <th>Difficulty</th>
-              <th className="text-left">Question / Statement</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((elm, index) => (
-              <tr key={index}>
-                <td className="text-center">{index + 1}</td>
-                <td>{decodeURIComponent(elm.category)}</td>
-                <td>
-                  {formatType(capitalizeString(decodeURIComponent(elm.type)))}
-                </td>
-                <td>{capitalizeString(decodeURIComponent(elm.difficulty))}</td>
-                <td>{decodeURIComponent(elm.question)}</td>
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th className="text-center">ID</th>
+                <th className="text-left">Category</th>
+                <th className="text-left">Type</th>
+                <th>Difficulty</th>
+                <th className="text-left">Question / Statement</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data
+                .slice((tablePage - 1) * 10, (tablePage - 1) * 10 + 10)
+                .map((elm, index) => (
+                  <tr key={index}>
+                    <td className="text-center">
+                      {(tablePage - 1) * 10 + index + 1}
+                    </td>
+                    <td>{decodeURIComponent(elm.category)}</td>
+                    <td>
+                      {formatType(
+                        capitalizeString(decodeURIComponent(elm.type))
+                      )}
+                    </td>
+                    <td>
+                      {capitalizeString(decodeURIComponent(elm.difficulty))}
+                    </td>
+                    <td>{decodeURIComponent(elm.question)}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+
+          {pages > 0 && (
+            <div className="table__pagination">
+              <div>
+                {tablePage > 1 && (
+                  <span onClick={() => setTablePage(tablePage - 1)}>
+                    <FaAngleLeft />
+                  </span>
+                )}
+                {Array.from({ length: pages }, (_, index) => index + 1).map(
+                  (elm, index) => (
+                    <span
+                      key={index}
+                      className={
+                        tablePage === elm ? "table__pagination--selected" : ""
+                      }
+                      onClick={() => setTablePage(elm)}
+                    >
+                      {elm}
+                    </span>
+                  )
+                )}
+                {tablePage < pages && (
+                  <span onClick={() => setTablePage(tablePage + 1)}>
+                    <FaAngleRight />
+                  </span>
+                )}
+              </div>
+            </div>
+          )}
+        </>
       ) : (
         <p>Cargando...</p>
       )}
